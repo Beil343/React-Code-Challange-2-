77b134bb80ec4192c15c933823098342bafbcaca
Gabriel <gabrielmuhoro@gmail.com>
2023-04-27 14:30:16 +0300

    third commit

--- react-code-challange-2/src/App.js
+++ react-code-challange-2/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import BotCollection from './components/BotCollection';
+import YourBotArmy from './components/YourBotArmy';
 
 function App() {
+  const [army, setArmy] = useState([]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Bot Battlr</h1>
       </header>
+      <main>
+        <BotCollection army={army} setArmy={setArmy} />
+        <YourBotArmy army={army} setArmy={setArmy} />
+      </main>
     </div>
   );
 }
